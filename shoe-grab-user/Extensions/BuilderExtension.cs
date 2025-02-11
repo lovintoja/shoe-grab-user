4966af355bade86a2bb8228100a43630e8deec46
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-11 11:30:40 +0100

    xxx-stash

--- shoe-grab-user/Extensions/BuilderExtension.cs
+++ shoe-grab-user/Extensions/BuilderExtension.cs
@@ -61,7 +61,7 @@ public static class BuilderExtension
             var grpcEndpoint = kestrelSection.GetSection("Grpc");
             if (grpcEndpoint.Exists())
             {
-                var grpcUrl = new Uri(grpcEndpoint["Url"]);
+                var grpcUrl = new Uri(Environment.GetEnvironmentVariable("USER_GRPC_URI"));
                 options.Listen(IPAddress.Parse(grpcUrl.Host), grpcUrl.Port, listenOptions =>
                 {
                     listenOptions.Protocols = Enum.Parse<HttpProtocols>(grpcEndpoint["Protocols"]);
@@ -84,7 +84,7 @@ public static class BuilderExtension
             var restApiEndpoint = kestrelSection.GetSection("RestApi");
             if (restApiEndpoint.Exists())
             {
-                var restApiUrl = new Uri(restApiEndpoint["Url"]);
+                var restApiUrl = new Uri(Environment.GetEnvironmentVariable("USER_REST_URI"));
                 options.Listen(IPAddress.Parse(restApiUrl.Host), restApiUrl.Port, listenOptions =>
                 {
                     listenOptions.Protocols = Enum.Parse<HttpProtocols>(restApiEndpoint["Protocols"]);
